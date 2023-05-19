d85e74f6a1a6319b698375d05ff61118eb3b90ba
xieyutia <sunnyyutian@outlook.com>
2023-05-18 23:39:29 -0700

    style comment

--- src/components/Component/NowVideo.jsx
+++ src/components/Component/NowVideo.jsx
@@ -1,6 +1,7 @@
 import headerImg from "../../assets/images/Mohan-muruge.jpg";
 import viewsIcon from "../../assets/Icons/views.svg";
 import likesIcon from "../../assets/Icons/likes.svg";
+import commentIcon from "../../assets/Icons/add_comment.svg";
 import { convertDate } from "./convertDate.js";
 
 let apiKey = "?api_key=<f61e3f42-45c7-4ae9-9acd-1525652bf2fd>";
@@ -34,7 +35,7 @@ function NowVideo(props) {
             </div>
           </div>
         </div>
-        <div>
+        <div className="nowVideo__description">
           <p>{props.nowVideo.description}</p>
         </div>
       </div>
@@ -42,10 +43,8 @@ function NowVideo(props) {
         <div>
           <h2>{props.nowVideo.comments.length} Comments</h2>
         </div>
-        <div>
-          <div id="mohan-img">
-            <img src={headerImg} alt="" />
-          </div>
+        <div className="nowVideo__comment-conversation">
+          <img src={headerImg} alt="" />
           <form
             action=""
             onSubmit={(event) => {
@@ -53,13 +52,13 @@ function NowVideo(props) {
               props.addComment(event.target.name.value);
             }}
           >
-            <div>
-              <label>JOIN THE CONVERSATION</label>
-              <input name="name" type="text" placeholder="Add a new comment" />
-            </div>
-            <div>
-              <button>COMMENT</button>
-            </div>
+            <label>JOIN THE CONVERSATION</label>
+            <textarea name="name" type="text" placeholder="Add a new comment" />
+
+            <button>
+              <img src={commentIcon} alt="" />
+              COMMENT
+            </button>
           </form>
         </div>
       </div>
