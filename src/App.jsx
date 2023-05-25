e24cd31e6a7e9a5d6e3b8f61a70620887fd8ec4a
xieyutia <sunnyyutian@outlook.com>
2023-05-25 16:02:51 -0700

    able to change path with id

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<Navigate to="/" />} />
-          <Route path="/videos" element={<Navigate to="/" />} />
+          <Route path="/videos" element={<HomePage />} />
+          <Route path="/videos/:id" element={<HomePage />} />
           <Route path="/uploads" element={<UploadPage />} />
         </Routes>
       </div>
--- src/components/Component/VideoList.jsx
+++ src/components/Component/VideoList.jsx
@@ -1,3 +1,5 @@
+import { Link, useParams } from "react-router-dom";
+
 function VideoList(props) {
   //   console.log(props);
   return (
@@ -10,22 +12,24 @@ function VideoList(props) {
           return video.id !== props.nowVideo.id;
         })
         .map((video) => (
-          <div
-            key={video.id}
-            onClick={() => {
-              props.handleVideoClick(video.id);
-            }}
-          >
-            <div className="videoList__wraper">
-              <div className="videoList__wraper-left">
-                <img src={video.image} alt="" />
-              </div>
-              <div className="videoList__wraper-right">
-                <h3>{video.title}</h3>
-                <p>{video.channel}</p>
+          <Link to={`/videos/${video.id}`}>
+            <div
+              key={video.id}
+              onClick={() => {
+                props.handleVideoClick(video.id);
+              }}
+            >
+              <div className="videoList__wraper">
+                <div className="videoList__wraper-left">
+                  <img src={video.image} alt="" />
+                </div>
+                <div className="videoList__wraper-right">
+                  <h3>{video.title}</h3>
+                  <p>{video.channel}</p>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
     </div>
   );
