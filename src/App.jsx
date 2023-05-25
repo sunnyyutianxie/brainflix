beacad5b12e07e73d97beeb0cfcad26f5c6dfd0d
xieyutia <sunnyyutian@outlook.com>
2023-05-25 15:58:53 -0700

    test api

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,6 @@ import UploadPage from "./components/Component/UploadPage";
 import { useParams } from "react-router-dom";
 
 function App() {
-  const params = useParams();
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://project-2-api.herokuapp.com/videos/?api_key=%3Cf61e3f42-45c7-4ae9-9acd-1525652bf2fd%3E"
-      )
-      .then((response) => {
-        // console.log(response.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  useEffect(() => {
-    // console.log(params.id);
-    if (params.id) {
-      axios
-        .get(
-          `https://project-2-api.herokuapp.com/videos/${params.id}/?api_key=%3Cf61e3f42-45c7-4ae9-9acd-1525652bf2fd%3E`
-        )
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [params.id]);
-
   return (
     <BrowserRouter>
       <div className="App">
@@ -43,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<Navigate to="/" />} />
-          <Route path="/video" element={<Navigate to="/" />} />
+          <Route path="/videos" element={<Navigate to="/" />} />
           <Route path="/uploads" element={<UploadPage />} />
         </Routes>
       </div>
--- src/components/Component/HomePage.jsx
+++ src/components/Component/HomePage.jsx
@@ -2,7 +2,9 @@ import VideoPlaying from "./VideoPlaying";
 import NowVideo from "./NowVideo";
 import CommentList from "./CommentList";
 import VideoList from "./VideoList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import videoDetails from "../../data/video-details.json";
 
 function HomePage() {
@@ -12,6 +14,33 @@ function HomePage() {
     const foundVideo = videos.find((video) => video.id === id);
     setNowVideo(foundVideo);
   };
+
+  const params = useParams();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://project-2-api.herokuapp.com/videos/?api_key=%3Cf61e3f42-45c7-4ae9-9acd-1525652bf2fd%3E"
+      )
+      .then((response) => {
+        // console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    // console.log(params.id);
+    if (params.id) {
+      axios
+        .get(
+          `https://project-2-api.herokuapp.com/videos/${params.id}/?api_key=%3Cf61e3f42-45c7-4ae9-9acd-1525652bf2fd%3E`
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [params.id]);
 
   //TODO: make it functional, update the comment
   function addComment(content) {
