8f1f7bcd7ff7257f027be203b85c0c4b3a5efb4d
xieyutia <sunnyyutian@outlook.com>
2023-05-25 12:50:13 -0700

    add axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import videoDetails from "./data/video-details.json";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Component/Header";
-import VideoList from "./components/Component/VideoList";
-import NowVideo from "./components/Component/NowVideo";
-import CommentList from "./components/Component/CommentList";
-import VideoPlaying from "./components/Component/VideoPlaying";
+import axios from "axios";
 import HomePage from "./components/Component/HomePage";
 import UploadPage from "./components/Component/UploadPage";
+import { Link, useParams } from "react-router-dom";
 
 function App() {
   const [videos, setVideos] = useState(videoDetails);
@@ -17,6 +15,33 @@ function App() {
     const foundVideo = videos.find((video) => video.id === id);
     setNowVideo(foundVideo);
   };
+
+  const params = useParams();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://project-2-api.herokuapp.com/videos/?api_key=%3Cf61e3f42-45c7-4ae9-9acd-1525652bf2fd%3E"
+      )
+      .then((response) => {
+        // console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    // console.log(params.id);
+    if (params.id) {
+      axios
+        .get(
+          `https://project-2-api.herokuapp.com/videos/${params.id}/?api_key=%3Cf61e3f42-45c7-4ae9-9acd-1525652bf2fd%3E`
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [params.id]);
 
   //TODO: make it functional, update the comment
   function addComment(content) {
@@ -34,28 +59,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/* Header Section */}
         <Header />
-
-        {/* Video Playing Section
-        <VideoPlaying nowVideo={nowVideo} /> */}
-
-        {/* <div class="container">
-          <div class="left-container">
-            {/* Now Video Section */}
-        {/* <NowVideo nowVideo={nowVideo} addComment={addComment} />
-
-            {/* Comment Section */}
-        {/* <CommentList comments={nowVideo.comments} />
-          </div>
-
-          {/* Next Video Section */}
-        {/* <VideoList
-            videos={videos}
-            nowVideo={nowVideo}
-            handleVideoClick={handleVideoClick}
-          />
-        </div> */}
 
         <Routes>
           <Route
